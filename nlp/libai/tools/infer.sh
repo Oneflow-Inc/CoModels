04432db2b32d03c558e67ea5b11f1bbb79a22e22
lifei <dr2218864812@gmail.com>
2023-10-12 04:44:46 +0000

    libai

---
+++ nlp/libai/tools/infer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+FILE=$1
+GPUS=$2
+NODE=${NODE:-1}
+NODE_RANK=${NODE_RANK:-0}
+ADDR=${ADDR:-127.0.0.1}
+PORT=${PORT:-12345}
+
+export ONEFLOW_LINEAR_EMBEDDING_SKIP_INIT=1
+
+python3 -m oneflow.distributed.launch \
+--nproc_per_node $GPUS --nnodes $NODE --node_rank $NODE_RANK --master_addr $ADDR --master_port $PORT \
+$FILE ${@:3}
+
---
+++ nlp/libai/tools/train.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+FILE=$1
+CONFIG=$2
+GPUS=$3
+NODE=${NODE:-1}
+NODE_RANK=${NODE_RANK:-0}
+ADDR=${ADDR:-127.0.0.1}
+PORT=${PORT:-12345}
+
+export ONEFLOW_FUSE_OPTIMIZER_UPDATE_CAST=true
+
+python3 -m oneflow.distributed.launch \
+--nproc_per_node $GPUS --nnodes $NODE --node_rank $NODE_RANK --master_addr $ADDR --master_port $PORT \
+$FILE --config-file $CONFIG ${@:4}
+
