d28b5daf65d1e22423fb5761c4d71477f8769bb2
iwkkk <keyuwei320@gmail.com>
2023-10-13 07:12:23 +0000

    update resnet50

---
+++ cv/classification/resnet50/ddp_inference.sh
@@ -0,0 +1,13 @@
+export PYTHONPATH=$PWD:$PYTHONPATH
+set -aux
+
+GPU_NUMS=8
+PORT=12346
+MODEL_ARCH="resnet50"  # "swin_tiny_patch4_window7_224"
+
+python3 -m oneflow.distributed.launch --nproc_per_node $GPU_NUMS --master_addr 127.0.0.1 --master_port $PORT  \
+        ../main.py --cfg ../configs/cnn_inference_settings.yaml --model_arch $MODEL_ARCH
+
+
+
+
---
+++ cv/classification/resnet50/ddp_training.sh
@@ -0,0 +1,11 @@
+export PYTHONPATH=$PWD:$PYTHONPATH
+set -aux
+
+GPU_NUMS=8
+PORT=12346
+MODEL_ARCH="resnet50"  # "swin_tiny_patch4_window7_224"
+
+python3 -m oneflow.distributed.launch --nproc_per_node $GPU_NUMS --master_addr 127.0.0.1 --master_port $PORT  \
+        ../main.py --cfg ../configs/cnn_default_settings.yaml --model_arch $MODEL_ARCH  --epochs 50
+
+
