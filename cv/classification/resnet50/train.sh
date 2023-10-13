7bfa157c32b325f69011eb2a32839e24ec9363c4
iwkkk <keyuwei320@gmail.com>
2023-10-13 08:10:22 +0000

    format command

--- cv/classification/resnet50/train.sh
+++ cv/classification/resnet50/train.sh
@@ -5,7 +5,14 @@ GPU_NUMS=8
 PORT=12346
 MODEL_ARCH="resnet50" 
 
-python3 -m oneflow.distributed.launch --nproc_per_node $GPU_NUMS --master_addr 127.0.0.1 --master_port $PORT  \
-        main.py --cfg configs/cnn_default_settings.yaml --model_arch $MODEL_ARCH  --epochs 50
+python3 -m oneflow.distributed.launch \
+    --nproc_per_node $GPU_NUMS \
+    --master_addr 127.0.0.1 \
+    --master_port $PORT  \
+    main.py \
+    --cfg configs/cnn_default_settings.yaml \
+    --model_arch $MODEL_ARCH \
+    --epochs 50
+
 
 
