1d9d19500ef8a8e1d9c9ae98c91489e70e45cf57
Kunyang Zhou <118790294+zkyseu@users.noreply.github.com>
2023-10-18 11:06:51 +0800

    Update train.py

--- cv/detection/train.py
+++ cv/detection/train.py
@@ -15,6 +15,8 @@ from group_by_aspect_ratio import GroupedBatchSampler, create_aspect_ratio_group
 import presets 
 import utils
 
+import retinanet_resnet50_fpn # Registry
+
 def get_dataset(name, image_set, transform, data_path):
     paths = {"coco": (data_path, get_coco, 91), "coco_kp": (data_path, get_coco_kp, 2)}
     p, ds_fn, num_classes = paths[name]
