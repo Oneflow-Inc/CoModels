4e725592481209b5c4707db99237c9a893f4a1f6
iwkkk <keyuwei320@gmail.com>
2023-10-20 09:23:15 +0000

    resnet34

---
+++ cv/classification/resnet34/train.sh
@@ -0,0 +1,16 @@
+export PYTHONPATH=$PWD:$PYTHONPATH
+set -aux
+
+GPU_NUMS=4
+PORT=12346
+MODEL_ARCH="resnet34"
+
+python3 -m oneflow.distributed.launch \
+        --nproc_per_node $GPU_NUMS \
+        --master_addr 127.0.0.1 \
+        --master_port $PORT \
+        main.py \
+        --cfg configs/cnn_default_settings_resnet.yaml \
+        --model_arch $MODEL_ARCH 
+
+        
