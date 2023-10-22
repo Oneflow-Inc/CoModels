535d39f06a810f0995b4547698bb67bf9d12f13e
iwkkk <keyuwei320@gmail.com>
2023-10-22 21:44:56 +0000

    add row

--- cv/classification/resnet18/infer.sh
+++ cv/classification/resnet18/infer.sh
@@ -14,3 +14,4 @@ python3 -m oneflow.distributed.launch \
         --model_arch $MODEL_ARCH \
         --throughput
 
+   
--- cv/classification/resnet50/train.sh
+++ cv/classification/resnet50/train.sh
@@ -12,3 +12,5 @@ python3 -m oneflow.distributed.launch \
         main.py \
         --cfg configs/resnet_settings.yaml \
         --model_arch $MODEL_ARCH 
+
+        
