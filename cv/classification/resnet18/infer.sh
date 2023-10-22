545a3a4bd94b69f992fd14b13ea8fe35ed6d630e
iwkkk <keyuwei320@gmail.com>
2023-10-22 21:30:06 +0000

    fix minor error

--- cv/classification/resnet18/infer.sh
+++ cv/classification/resnet18/infer.sh
@@ -5,7 +5,7 @@ GPU_NUMS=1
 PORT=12346
 MODEL_ARCH="resnet18"
 
-python3 -m oneflow.distributed.lasunch \
+python3 -m oneflow.distributed.launch \
         --nproc_per_node $GPU_NUMS \
         --master_addr 127.0.0.1 \
         --master_port $PORT \
