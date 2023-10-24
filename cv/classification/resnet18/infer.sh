499a8c73f4548020a7ab6600588809e3e09dece4
ShawnXuan <xiexuanx2@gmail.com>
2023-10-24 01:38:39 +0000

    update

--- cv/classification/resnet18/infer.sh
+++ cv/classification/resnet18/infer.sh
@@ -8,7 +8,7 @@ MODEL_ARCH="resnet18"
 python3 -m oneflow.distributed.launch \
         --nproc_per_node $GPU_NUMS \
         --master_addr 127.0.0.1 \
-        --master_port $PORT \ 
+        --master_port $PORT \
         main.py \
         --cfg configs/resnet_settings.yaml \
         --model_arch $MODEL_ARCH \
