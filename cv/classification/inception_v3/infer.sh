d29a167049f48f4502c17827234d84a345f8fac9
akeeei <z13101735107@163.com>
2023-10-25 06:56:03 +0000

    config

--- cv/classification/inception_v3/infer.sh
+++ cv/classification/inception_v3/infer.sh
@@ -10,5 +10,6 @@ python3 -m oneflow.distributed.launch \
         --master_addr 127.0.0.1 \
         --master_port $PORT \
         main.py \
-        --cfg configs/inception_v3_inference_settings.yaml \
-        --model_arch $MODEL_ARCH
+        --cfg configs/inception_v3_default_settings.yaml \
+        --model_arch $MODEL_ARCH \
+        --throughput
