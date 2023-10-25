c5311c8e87f975da3ade958364d5019f27c83d22
lifei <dr2218864812@gmail.com>
2023-10-25 11:20:11 +0800

    VisionTransformer

---
+++ nlp/libai/VisionTransformer/vit_infer_config.py
@@ -0,0 +1,9 @@
+from .vim_imagenet import train, optim, model, dataloader, graph
+import flowvision.transforms as transforms
+from flowvision.transforms import InterpolationMode
+from flowvision.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+from flowvision.data import Mixup
+from flowvision.loss.cross_entropy import SoftTargetCrossEntropy
+
+
+train.load_weight = "./output_vim/model_0000199"
