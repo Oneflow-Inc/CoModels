d28a56f9810bd33a09d2be24650e187c04550708
zkyseu <220221888@seu.edu.cn>
2023-10-26 11:24:33 +0800

    dev_deeplabv3_resnet101

--- cv/segment/deeplabv3_resnet101_coco/infer.sh
+++ cv/segment/deeplabv3_resnet101_coco/infer.sh
@@ -2,7 +2,7 @@
     -b 24 \
     --dataset coco \
     --data-path /dataset/coco \
-    --model deeplabv3_mobilenet_v3_large_coco \
+    --model deeplabv3_resnet101_coco \
     --aux-loss \
     --lr 0.12 \
     --pretrained \
--- cv/segment/deeplabv3_resnet101_coco/train.sh
+++ cv/segment/deeplabv3_resnet101_coco/train.sh
@@ -2,5 +2,5 @@ python train.py \
     -b 24 \
     --dataset coco \
     --data-path /dataset/coco \
-    --model deeplabv3_mobilenet_v3_large_coco \
+    --model deeplabv3_resnet101_coco \
     --aux-loss
