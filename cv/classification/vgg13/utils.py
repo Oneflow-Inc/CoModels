5f1636cf02f1fba0e81b776f0944e945f5164c8e
zkyseu <220221888@seu.edu.cn>
2023-10-26 10:19:27 +0800

    dev_deeplabv3_mobile

--- cv/classification/vgg13/utils.py
+++ cv/classification/vgg13/utils.py
@@ -1,1 +1,153 @@
-../utils.py
+"""
+Modified from https://github.com/microsoft/Swin-Transformer/blob/main/utils.py
+"""
+
+import os
+import oneflow as flow
+from flowvision.models.utils import load_state_dict_from_url
+import time
+
+
+def load_checkpoint(config, model, optimizer, lr_scheduler, logger):
+    logger.info(
+        f"==============> Resuming form {config.MODEL.RESUME}...................."
+    )
+    if config.MODEL.RESUME.startswith("https"):
+        checkpoint = load_state_dict_from_url(
+            config.MODEL.RESUME, map_location="cpu", check_hash=True
+        )
+    else:
+        checkpoint = flow.load(config.MODEL.RESUME)
+    msg = model.load_state_dict(checkpoint["model"], strict=False)
+    logger.info(msg)
+    max_accuracy = 0.0
+    if (
+        not config.EVAL_MODE
+        and "optimizer" in checkpoint
+        and "lr_scheduler" in checkpoint
+        and "epoch" in checkpoint
+    ):
+        optimizer.load_state_dict(checkpoint["optimizer"])
+        lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
+        config.defrost()
+        config.TRAIN.START_EPOCH = checkpoint["epoch"] + 1
+        config.freeze()
+        logger.info(
+            f"=> loaded successfully '{config.MODEL.RESUME}' (epoch {checkpoint['epoch']})"
+        )
+        if "max_accuracy" in checkpoint:
+            max_accuracy = checkpoint["max_accuracy"]
+
+    return max_accuracy
+
+
+def save_checkpoint(
+    config, epoch, model, max_accuracy, optimizer, lr_scheduler, logger
+):
+    save_state = {
+        "model": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+        "lr_scheduler": lr_scheduler.state_dict(),
+        "max_accuracy": max_accuracy,
+        "epoch": epoch,
+        "config": config,
+    }
+
+    save_path = os.path.join(config.OUTPUT, f"model_{epoch}")
+    logger.info(f"{save_path} saving......")
+    flow.save(save_state, save_path)
+    logger.info(f"{save_path} saved !!!")
+
+
+def get_grad_norm(parameters, norm_type=2):
+    total_norm = flow.linalg.vector_norm(
+        flow.stack(
+            [flow.linalg.vector_norm(p.grad.detach(), norm_type) for p in parameters]
+        ),
+        norm_type,
+    )
+    return total_norm
+
+
+def auto_resume_helper(output_dir):
+    checkpoints = os.listdir(output_dir)
+    checkpoints = [
+        ckpt for ckpt in checkpoints if os.path.isdir(os.path.join(output_dir, ckpt))
+    ]
+    print(f"All checkpoints founded in {output_dir}: {checkpoints}")
+    if len(checkpoints) > 0:
+        latest_checkpoint = max(
+            [os.path.join(output_dir, d) for d in checkpoints], key=os.path.getmtime
+        )
+        print(f"The latest checkpoint founded: {latest_checkpoint}")
+        resume_file = latest_checkpoint
+    else:
+        resume_file = None
+    return resume_file
+
+
+def reduce_tensor(tensor):
+    rt = tensor.clone()
+    flow.comm.all_reduce(rt)
+    rt /= flow.env.get_world_size()
+    return rt
+
+class AverageMeter:
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def record(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+    
+    def get(self):
+        if isinstance(self.val, flow.Tensor):
+            return self.val.item(), self.avg.item()
+        else:
+            return self.val, self.avg
+
+
+class TimeMeter(object):
+    def __init__(self, return_timestamp=False):
+        self.return_timestamp = return_timestamp
+        self.total_n = 0
+        self.n = 0
+        self.start_time = None
+        self.ets = None
+        self.bts = None
+        self.reset()
+
+    def reset(self):
+        self.n = 0
+        if self.start_time is None:
+            self.start_time = time.perf_counter()
+        if self.ets is None:
+            self.bts = time.perf_counter()
+        else:
+            self.bts = self.ets
+        self.ets = None
+
+    def record(self, n):
+        self.n += n
+        self.total_n += n
+
+    def get(self):
+        self.ets = time.perf_counter()
+        assert self.ets > self.bts, f"{self.ets} > {self.bts}"
+        assert self.ets > self.start_time, f"{self.ets} > {self.start_time}"
+        throughput = self.n / (self.ets - self.bts)
+        avg_throughput = self.total_n / (self.ets - self.start_time)
+        if self.return_timestamp:
+            return throughput, avg_throughput, self.ets
+        else:
+            return throughput, avg_throughput
--- cv/segment/fcn_resnet50_coco/utils.py
+++ cv/segment/fcn_resnet50_coco/utils.py
@@ -1,1 +1,292 @@
-../utils.py
+import datetime
+import errno
+import os
+import time
+from collections import defaultdict, deque
+
+import oneflow
+import oneflow.distributed as dist
+
+
+class SmoothedValue:
+    """Track a series of values and provide access to smoothed values over a
+    window or the global series average.
+    """
+
+    def __init__(self, window_size=20, fmt=None):
+        if fmt is None:
+            fmt = "{median:.4f} ({global_avg:.4f})"
+        self.deque = deque(maxlen=window_size)
+        self.total = 0.0
+        self.count = 0
+        self.fmt = fmt
+
+    def update(self, value, n=1):
+        self.deque.append(value)
+        self.count += n
+        self.total += value * n
+
+    def synchronize_between_processes(self):
+        """
+        Warning: does not synchronize the deque!
+        """
+        t = reduce_across_processes([self.count, self.total])
+        t = t.tolist()
+        self.count = int(t[0])
+        self.total = t[1]
+
+    @property
+    def median(self):
+        d = oneflow.tensor(list(self.deque))
+        return d.median().item()
+
+    @property
+    def avg(self):
+        d = oneflow.tensor(list(self.deque), dtype=oneflow.float32)
+        return d.mean().item()
+
+    @property
+    def global_avg(self):
+        return self.total / self.count
+
+    @property
+    def max(self):
+        return max(self.deque)
+
+    @property
+    def value(self):
+        return self.deque[-1]
+
+    def __str__(self):
+        return self.fmt.format(
+            median=self.median, avg=self.avg, global_avg=self.global_avg, max=self.max, value=self.value
+        )
+
+
+class ConfusionMatrix:
+    def __init__(self, num_classes):
+        self.num_classes = num_classes
+        self.mat = None
+
+    def update(self, a, b):
+        n = self.num_classes
+        if self.mat is None:
+            self.mat = oneflow.zeros((n, n), dtype=oneflow.int64, device=a.device)
+        with oneflow.no_grad():
+            k = (a >= 0) & (a < n)
+            inds = n * a[k].to(oneflow.int64) + b[k]
+            self.mat += oneflow.bincount(inds, minlength=n**2).reshape(n, n)
+
+    def reset(self):
+        self.mat.zero_()
+
+    def compute(self):
+        h = self.mat.float()
+        acc_global = oneflow.diag(h).sum() / h.sum()
+        acc = oneflow.diag(h) / h.sum(1)
+        iu = oneflow.diag(h) / (h.sum(1) + h.sum(0) - oneflow.diag(h))
+        return acc_global, acc, iu
+
+    def reduce_from_all_processes(self):
+        self.mat = reduce_across_processes(self.mat).to(oneflow.int64)
+
+    def __str__(self):
+        acc_global, acc, iu = self.compute()
+        return ("global correct: {:.1f}\naverage row correct: {}\nIoU: {}\nmean IoU: {:.1f}").format(
+            acc_global.item() * 100,
+            [f"{i:.1f}" for i in (acc * 100).tolist()],
+            [f"{i:.1f}" for i in (iu * 100).tolist()],
+            iu.mean().item() * 100,
+        )
+
+
+class MetricLogger:
+    def __init__(self, delimiter="\t"):
+        self.meters = defaultdict(SmoothedValue)
+        self.delimiter = delimiter
+
+    def update(self, **kwargs):
+        for k, v in kwargs.items():
+            if isinstance(v, oneflow.Tensor):
+                v = v.item()
+            if not isinstance(v, (float, int)):
+                raise TypeError(
+                    f"This method expects the value of the input arguments to be of type float or int, instead  got {type(v)}"
+                )
+            self.meters[k].update(v)
+
+    def __getattr__(self, attr):
+        if attr in self.meters:
+            return self.meters[attr]
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        raise AttributeError(f"'{type(self).__name__}' object has no attribute '{attr}'")
+
+    def __str__(self):
+        loss_str = []
+        for name, meter in self.meters.items():
+            loss_str.append(f"{name}: {str(meter)}")
+        return self.delimiter.join(loss_str)
+
+    def synchronize_between_processes(self):
+        for meter in self.meters.values():
+            meter.synchronize_between_processes()
+
+    def add_meter(self, name, meter):
+        self.meters[name] = meter
+
+    def log_every(self, iterable, print_freq, header=None):
+        i = 0
+        if not header:
+            header = ""
+        start_time = time.time()
+        end = time.time()
+        iter_time = SmoothedValue(fmt="{avg:.4f}")
+        data_time = SmoothedValue(fmt="{avg:.4f}")
+        space_fmt = ":" + str(len(str(len(iterable)))) + "d"
+        if oneflow.cuda.is_available():
+            log_msg = self.delimiter.join(
+                [
+                    header,
+                    "[{0" + space_fmt + "}/{1}]",
+                    "eta: {eta}",
+                    "{meters}",
+                    "time: {time}",
+                    "data: {data}",
+                ]
+            )
+        else:
+            log_msg = self.delimiter.join(
+                [header, "[{0" + space_fmt + "}/{1}]", "eta: {eta}", "{meters}", "time: {time}", "data: {data}"]
+            )
+        MB = 1024.0 * 1024.0
+        for obj in iterable:
+            data_time.update(time.time() - end)
+            yield obj
+            iter_time.update(time.time() - end)
+            if i % print_freq == 0:
+                eta_seconds = iter_time.global_avg * (len(iterable) - i)
+                eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+                if oneflow.cuda.is_available():
+                    print(
+                        log_msg.format(
+                            i,
+                            len(iterable),
+                            eta=eta_string,
+                            meters=str(self),
+                            time=str(iter_time),
+                            data=str(data_time),
+                        )
+                    )
+                else:
+                    print(
+                        log_msg.format(
+                            i, len(iterable), eta=eta_string, meters=str(self), time=str(iter_time), data=str(data_time)
+                        )
+                    )
+            i += 1
+            end = time.time()
+        total_time = time.time() - start_time
+        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+        print(f"{header} Total time: {total_time_str}")
+
+
+def cat_list(images, fill_value=0):
+    max_size = tuple(max(s) for s in zip(*[img.shape for img in images]))
+    batch_shape = (len(images),) + max_size
+    batched_imgs = images[0].new(*batch_shape).fill_(fill_value)
+    for img, pad_img in zip(images, batched_imgs):
+        pad_img[..., : img.shape[-2], : img.shape[-1]].copy_(img)
+    return batched_imgs
+
+
+def collate_fn(batch):
+    images, targets = list(zip(*batch))
+    batched_imgs = cat_list(images, fill_value=0)
+    batched_targets = cat_list(targets, fill_value=255)
+    return batched_imgs, batched_targets
+
+
+def mkdir(path):
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
+
+def setup_for_distributed(is_master):
+    """
+    This function disables printing when not in master process
+    """
+    import builtins as __builtin__
+
+    builtin_print = __builtin__.print
+
+    def print(*args, **kwargs):
+        force = kwargs.pop("force", False)
+        if is_master or force:
+            builtin_print(*args, **kwargs)
+
+    __builtin__.print = print
+
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    return True
+
+
+def get_world_size():
+    if not is_dist_avail_and_initialized():
+        return 1
+    return dist.get_world_size()
+
+
+def get_rank():
+    if not is_dist_avail_and_initialized():
+        return 0
+    return dist.get_rank()
+
+
+def is_main_process():
+    return get_rank() == 0
+
+
+def save_on_master(*args, **kwargs):
+    if is_main_process():
+        oneflow.save(*args, **kwargs)
+
+
+def init_distributed_mode(args):
+    if "RANK" in os.environ and "WORLD_SIZE" in os.environ:
+        args.rank = int(os.environ["RANK"])
+        args.world_size = int(os.environ["WORLD_SIZE"])
+        args.gpu = int(os.environ["LOCAL_RANK"])
+    elif "SLURM_PROCID" in os.environ:
+        args.rank = int(os.environ["SLURM_PROCID"])
+        args.gpu = args.rank % flow.cuda.device_count()
+    else:
+        print("No using distributed mode")
+        args.distributed = False
+        return
+
+    if args.world_size == 1:
+        args.distributed = False
+        return
+
+    args.distributed = True
+    setup_for_distributed(args.rank == 0)
+
+
+def reduce_across_processes(val):
+    if not is_dist_avail_and_initialized():
+        # nothing to sync, but we still convert to tensor for consistency with the distributed case.
+        return oneflow.tensor(val)
+
+    t = oneflow.tensor(val, device="cuda")
+    oneflow.comm.barrier()
+    oneflow.comm.all_reduce(t)
+    return t
