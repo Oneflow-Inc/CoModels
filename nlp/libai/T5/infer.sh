01fd387fa5e4692e235bf42421e2cb42c3460733
lifei <dr2218864812@gmail.com>
2023-10-27 11:55:54 +0800

    ln

--- nlp/libai/T5/infer.sh
+++ nlp/libai/T5/infer.sh
@@ -9,6 +9,6 @@ python3 -m oneflow.distributed.launch \
         --master_addr 127.0.0.1\
         --master_port 12345 \
         train_net.py \
-        --config-file T5/t5_infer_config.py \
+        --config-file t5_infer_config.py \
 	--eval-only 
 
