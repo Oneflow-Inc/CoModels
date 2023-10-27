8a552f2a4bd5b4ad7126556faebf78e124910f59
zkyseu <220221888@seu.edu.cn>
2023-10-27 14:36:24 +0800

    add deeplabv3_r101

---
+++ cv/segment/deeplabv3_resnet101_coco/infer.sh
@@ -0,0 +1,10 @@
+ python train.py \
+    -b 24 \
+    --dataset coco \
+    --data-path /dataset/coco \
+    --model deeplabv3_resnet101_coco \
+    --aux-loss \
+    --lr 0.12 \
+    --pretrained \
+    --test-only
+    
