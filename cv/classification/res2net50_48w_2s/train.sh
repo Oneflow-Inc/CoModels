29e46fe7d0f16d6da4608d4c2efb1e68488f0d62
iwkkk <keyuwei320@gmail.com>
2023-10-30 07:45:46 +0000

    add res2net

---
+++ cv/classification/res2net50_48w_2s/train.sh
@@ -0,0 +1,15 @@
+export PYTHONPATH=$PWD:$PYTHONPATH
+set -aux
+
+GPU_NUMS=8
+PORT=12346
+MODEL_ARCH="res2net50_48w_2s"
+
+python3 -m oneflow.distributed.launch \
+        --nproc_per_node $GPU_NUMS \
+        --master_addr 127.0.0.1 \
+        --master_port $PORT \
+        main.py \
+        --cfg configs/default_settings.yaml \
+        --model_arch $MODEL_ARCH 
+
