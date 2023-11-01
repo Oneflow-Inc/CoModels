4530202c3cad1c9a442221da8a6a6ded067225ca
lifei <dr2218864812@gmail.com>
2023-11-01 06:53:17 +0000

    GENet

---
+++ cv/classification/genet/train.sh
@@ -0,0 +1,16 @@
+export PYTHONPATH=$PWD:$PYTHONPATH
+set -aux
+
+GPU_NUMS=8
+PORT=12346
+MODEL_ARCH="genet_large"
+
+python3 -m oneflow.distributed.launch \
+        --nproc_per_node $GPU_NUMS \
+        --master_addr 127.0.0.1 \
+        --master_port $PORT \
+        main.py \
+        --cfg configs/senet_default_settings.yaml \
+        --model_arch $MODEL_ARCH \
+        --lr 1e-2
+
