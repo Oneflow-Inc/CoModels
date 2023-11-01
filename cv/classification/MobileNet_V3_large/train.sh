fa8acad3b7a039b9f194e6684f2819ddf820412a
cuirunxi <953131037@qq.com>
2023-11-01 07:23:49 +0000

    MobileNet_V3_large

---
+++ cv/classification/MobileNet_V3_large/train.sh
@@ -0,0 +1,16 @@
+export PYTHONPATH=$PWD:$PYTHONPATH
+set -aux
+
+GPU_NUMS=8
+PORT=12377
+MODEL_ARCH="mobilenet_v3_large"
+IMAGE_SIZE=224
+
+python3 -m oneflow.distributed.launch \
+        --nproc_per_node $GPU_NUMS \
+        --master_addr 127.0.0.1 \
+        --master_port $PORT \
+        main.py \
+        --cfg configs/resnest_default_settings.yaml \
+        --model_arch $MODEL_ARCH \
+        --image-size $IMAGE_SIZE
