7049445014201e7275dc899ec3e61db1e984d30a
Kunyang Zhou <118790294+zkyseu@users.noreply.github.com>
2023-11-02 10:33:12 +0800

    Update val.sh

--- cv/Face_Recognition/val.sh
+++ cv/Face_Recognition/val.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python val.py configs/ms1mv3_r50 --model_path output_ckpt/epoch_0
+# Using our provided weight 
+python val.py configs/ms1mv3_r50 --pretrained
+
+# Or using your trained weight
+# python val.py configs/ms1mv3_r50 --model_path output_ckpt/epoch_0
