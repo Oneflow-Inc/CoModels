78164f4d54acb3a7579eba69655b17292d123964
Kunyang Zhou <118790294+zkyseu@users.noreply.github.com>
2023-11-02 10:39:10 +0800

    Update val.sh

--- cv/Face_Recognition/iresnet101/val.sh
+++ cv/Face_Recognition/iresnet101/val.sh
@@ -1,1 +1,7 @@
-../val.sh
+#!/bin/bash
+
+# Using our provided weight 
+python val.py configs/ms1mv3_r101 --pretrained
+
+# Or using your trained weight
+# python val.py configs/ms1mv3_r101 --model_path output_ckpt/epoch_0
