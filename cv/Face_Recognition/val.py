22969103b74ebe441c0e0d106924b4118fd77ea4
Kunyang Zhou <118790294+zkyseu@users.noreply.github.com>
2023-11-02 10:31:29 +0800

    Update val.py

--- cv/Face_Recognition/val.py
+++ cv/Face_Recognition/val.py
@@ -5,7 +5,7 @@ from graph import TrainGraph, EvalGraph
 import logging
 import argparse
 from utils.utils_config import get_config
-
+from load_model import load_state_dict_from_url
 
 def main(args):
 
@@ -19,7 +19,14 @@ def main(args):
     val_callback = CallBackVerification(
         1, 0, cfg.val_targets, cfg.ofrecord_path)
 
-    state_dict = flow.load(args.model_path)
+    if args.pretrained:
+         if cfg.network == 'r50':
+             url='http://oneflow-public.oss-cn-beijing.aliyuncs.com/model_zoo/flowvision/face_recognition/iresnet50.zip'
+         elif cfg.network == 'r100':
+             url='http://oneflow-public.oss-cn-beijing.aliyuncs.com/model_zoo/flowvision/face_recognition/iresnet101.zip'
+         state_dict = load_state_dict_from_url(url)
+    else:
+         state_dict = flow.load(args.model_path)
 
     new_parameters = dict()
     for key, value in state_dict.items():
@@ -40,4 +47,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="OneFlow ArcFace val")
     parser.add_argument("config", type=str, help="py config file")
     parser.add_argument("--model_path", type=str, help="model path")
+    parser.add_argument(
+        "--pretrained",
+        dest="pretrained",
+        help="Use pre-trained models from the modelzoo",
+        action="store_true",
+    )
     main(parser.parse_args())
