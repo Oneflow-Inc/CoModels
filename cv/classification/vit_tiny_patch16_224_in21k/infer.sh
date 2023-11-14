ba814259d294f1b492a704e58fb540deca4dd5a5
akeeei <z13101735107@163.com>
2023-11-14 10:05:02 +0000

    vit_tiny_patch16_224_in21k

---
+++ cv/classification/vit_tiny_patch16_224_in21k/infer.sh
@@ -0,0 +1,17 @@
+export PYTHONPATH=$PWD:$PYTHONPATH
+set -aux
+
+GPU_NUMS=1
+PORT=12346
+MODEL_ARCH="vit_tiny_patch16_224_in21k"
+RESUME="output/vit_tiny_patch16_224_in21k/default/model_1"
+
+python3 -m oneflow.distributed.launch \
+        --nproc_per_node $GPU_NUMS \
+        --master_addr 127.0.0.1 \
+        --master_port $PORT \
+        main.py \
+        --cfg configs/vit_settings.yaml \
+        --model_arch $MODEL_ARCH \
+        --resume $RESUME \
+        --throughput
