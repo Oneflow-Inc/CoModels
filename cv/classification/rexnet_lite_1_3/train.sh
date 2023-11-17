1240c4f57346859a4090dfa73af1d1db54e8f829
cuirunxi <953131037@qq.com>
2023-11-17 03:33:36 +0000

    rexnet_lite_1_3

---
+++ cv/classification/rexnet_lite_1_3/train.sh
@@ -0,0 +1,15 @@
+export PYTHONPATH=$PWD:$PYTHONPATH
+set -aux
+
+GPU_NUMS=8
+PORT=12346
+MODEL_ARCH="rexnet_lite_1_3"
+
+python3 -m oneflow.distributed.launch \
+        --nproc_per_node $GPU_NUMS \
+        --master_addr 127.0.0.1 \
+        --master_port $PORT \
+        main.py \
+        --cfg configs/rexnet_settings.yaml \
+        --model_arch $MODEL_ARCH 
+
