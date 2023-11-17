3a5f0b1b424d9be2b0c14cc2f200114763c3dc98
iwkkk <keyuwei320@gmail.com>
2023-11-17 21:20:40 +0000

    add dla102

---
+++ cv/classification/dla102/infer.sh
@@ -0,0 +1,17 @@
+export PYTHONPATH=$PWD:$PYTHONPATH
+set -aux
+
+GPU_NUMS=1
+PORT=12346
+MODEL_ARCH="dla102"
+
+python3 -m oneflow.distributed.launch \
+        --nproc_per_node $GPU_NUMS \
+        --master_addr 127.0.0.1 \
+        --master_port $PORT \
+        main.py \
+        --cfg configs/default_settings.yaml \
+        --model_arch $MODEL_ARCH \
+        --lr 1e-3 \
+        --throughput
+
