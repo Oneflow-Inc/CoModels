0a5b6fb62ddc123d460f4ae5eb52a00110329ca4
akeeei <z13101735107@163.com>
2023-11-20 09:24:09 +0000

    train

--- cv/classification/fan_vit_tiny/train.sh
+++ cv/classification/fan_vit_tiny/train.sh
@@ -7,7 +7,6 @@ MODEL_ARCH="fan_tiny_12_p16_224"
 BATCH_SIZE=32
 LEARNING_RATE=1e-4
 
-export CUDA_VISIBLE_DEVICES=0,5,6,7
 python3 -m oneflow.distributed.launch \
         --nproc_per_node $GPU_NUMS \
         --master_addr 127.0.0.1 \
