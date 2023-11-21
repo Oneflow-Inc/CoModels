355c77d7bfc7105d693f88fd65948135bbf6a60d
akeeei <z13101735107@163.com>
2023-11-21 07:46:47 +0000

    fan_large_16_p4_hybrid_in22k_1k_384

--- cv/classification/fan_large_16_p4_hybrid_in22k_1k_384/train.sh
+++ cv/classification/fan_large_16_p4_hybrid_in22k_1k_384/train.sh
@@ -1,13 +1,13 @@
 export PYTHONPATH=$PWD:$PYTHONPATH
 set -aux
 
-GPU_NUMS=1
+GPU_NUMS=4
 PORT=12346
 MODEL_ARCH="fan_large_16_p4_hybrid_in22k_1k_384"
 IMAGE_SIZE=384
 BATCH_SIZE=8
 LEARNING_RATE=1e-4
-export CUDA_VISIBLE_DEVICES=3
+
 python3 -m oneflow.distributed.launch \
         --nproc_per_node $GPU_NUMS \
         --master_addr 127.0.0.1 \
